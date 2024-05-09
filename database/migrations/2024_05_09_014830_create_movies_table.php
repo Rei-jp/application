21405ff7c19b027984fa1cd79c160f8505e28235
Rei-jp <reo2003yoshide@icloud.com>
2024-05-09 13:05:46 +0900

    first commit

---
+++ database/migrations/2024_05_09_014830_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->char('title',50);
+            $table->integer('game_category_id');
+            $table->intger('stremer_id');
+            $table->text('URL');
+            $table->intger('user_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
